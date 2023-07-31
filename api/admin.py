ab417bdd1ee96976f984fb5cfbe7ed1f41f3830b
brenolima03 <brenocslima@outlook.com>
2023-07-31 13:46:11 -0400

    Item de pedido adicionado

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Pedido
+from .models import Produto, Pedido, ItemPedido
 
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'valor')
+
+@admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'data', 'valor_total')
     readonly_fields = ('valor_total',)
+    inlines = [ItemPedidoInline]
 
-admin.site.register(Pedido, PedidoAdmin)
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    pass
--- api/models.py
+++ api/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 
+class Produto(models.Model):
+    nome = models.CharField(max_length=20)
+    valor = models.FloatField()
+
+    def __str__(self):
+        return self.nome
+
 class Pedido(models.Model):
     data = models.DateField()
     cliente = models.CharField(max_length=20)
-    produtos = models.CharField(max_length=20)
-    valor = models.FloatField()
-    quantidade = models.IntegerField()
-    
+    produtos = models.ManyToManyField(Produto, through='ItemPedido')
+
     @property
     def valor_total(self):
-        if self.valor is not None and self.quantidade is not None:
-            return self.valor * self.quantidade
-        else:
-            return 0
-    
+        total_pedido = 0
+        for item_pedido in self.itempedido_set.all():
+            total_pedido += item_pedido.valor_total_item
+        return round(total_pedido, 2)
+
     def __str__(self):
-        return self.cliente 
+        return self.cliente
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=0)
+
+    @property
+    def valor_total_item(self):
+        return self.produto.valor * self.quantidade
